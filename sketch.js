2f7f296f0896f52c4b9989f5533fb2300aa8ea65
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-11-22 17:24:39 -0300

    22/11

--- sketch.js
+++ sketch.js
@@ -1,29 +1,74 @@
 var Rexinho, RexinhoCorrendo;
+
 var lugardopezinho, lugardopezinhochaozinho;
+
+var chaozinhoinvisivel;
+
+
 function preload(){
-RexinhoCorrendo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-lugardopezinhochaozinho = loadImage("ground2.png");
+
+    RexinhoCorrendo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+
+    lugardopezinhochaozinho = loadImage("ground2.png");
+
 }
+
+
 function setup(){
-createCanvas(600,200);
-Rexinho = createSprite(50,160,20,50);
-Rexinho.addAnimation("correndo", RexinhoCorrendo);
-Rexinho.scale = 0.5;
-lugardopezinho= createSprite(200, 180, 400, 20);
-lugardopezinho.addImage("pezinhodolugar", lugardopezinhochaozinho)
-lugardopezinho.x = lugardopezinho.width / 2;
-borda = createEdgeSprites();
+    createCanvas(600,200);
+
+    Rexinho = createSprite(50,160,20,50);
+
+    Rexinho.addAnimation("correndo", RexinhoCorrendo);
+
+    Rexinho.scale = 0.5;
+
+    lugardopezinho= createSprite(200, 180, 400, 20);
+
+    lugardopezinho.addImage("pezinhodolugar", lugardopezinhochaozinho)
+
+    lugardopezinho.x = lugardopezinho.width / 2;
+
+    borda = createEdgeSprites();
+
+    chaozinhoinvisivel = createSprite(100, 190, 400, 10); 
+
+    chaozinhoinvisivel.visible = false;
+
+    //var numero = Math.round(random(1,50));
+
+    //console.log(numero);
 }
+
+
 function draw(){
-background("white");
-lugardopezinho.velocityX = -2
-if (lugardopezinho.x < 0){
-lugardopezinho.x = lugardopezinho.width / 2;
+
+    background("white");
+
+    //console.log (Rexinho.y);
+
+    lugardopezinho.velocityX = -2
+    
+    if (lugardopezinho.x < 0){
+
+        lugardopezinho.x = lugardopezinho.width / 2;
+    }
+
+    if(keyDown("space") && Rexinho.y >= 150){
+
+        Rexinho.velocityY = -10;
+
+    }
+    Rexinho.velocityY = Rexinho.velocityY + 1;
+
+    Rexinho.collide(chaozinhoinvisivel);
+
+    nuvenhador();
+
+    drawSprites();
+
 }
-if(keyDown("space")){
-Rexinho.velocityY = -10;
-}
-Rexinho.velocityY = Rexinho.velocityY + 1;
-Rexinho.collide(lugardopezinho);
-drawSprites();
+
+function nuvenhador(){
+
 }
