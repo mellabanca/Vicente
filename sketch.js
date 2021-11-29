ca0d21619ee20ecdf1d4edf6b3debd8673bf7c9f
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-11-29 17:26:29 -0300

    29/11

--- sketch.js
+++ sketch.js
@@ -4,9 +4,20 @@ var lugardopezinho, lugardopezinhochaozinho;
 
 var chaozinhoinvisivel;
 
-var umanuvenzinha;
+var umanuvenzinha, grupodanuvenzinha;
 
 var inuvenzinha;
+
+var b1, b2, b3, b4, b5, b6, grudodosobstaculos;
+
+var pontinho = 0;
+
+var jogandinho = 1;
+
+var naumjogandinho = 0;
+
+var estadoJogandinho = jogandinho;
+
 
 function preload(){
 
@@ -15,10 +26,24 @@ function preload(){
     lugardopezinhochaozinho = loadImage("ground2.png");
 
     inuvenzinha = loadImage("cloud.png");
+
+    b1 = loadImage("obstacle1.png");
+
+    b2 = loadImage("obstacle2.png");
+
+    b3 = loadImage("obstacle3.png");
+
+    b4 = loadImage("obstacle4.png");
+
+    b5 = loadImage("obstacle5.png");
+
+    b6 = loadImage("obstacle6.png");
+
 }
 
 
 function setup(){
+
     createCanvas(600,200);
 
     Rexinho = createSprite(50,160,20,50);
@@ -44,6 +69,11 @@ function setup(){
     //console.log(numero);
 
     //console.log("Oi"+5);
+
+    grupodanuvenzinha = new Group();
+
+    grudodosobstaculos = new Group();
+
 }
 
 
@@ -51,11 +81,23 @@ function draw(){
 
     background("white");
 
+    text ("mortinhas:"+pontinho, 501, 51);
+
+    pontinho = pontinho+Math.round(frameCount/60);
+    
+    if(estadoJogandinho === jogandinho){
+
+        lugardopezinho.velocityX = -2;
+
+    } else if(estadoJogandinho === naumjogandinho){
+
+        lugardopezinho.velocityX = 0;
+
+    }
+
     //console.log (Rexinho.y);
 
     //console.log (frameCount);
-    
-    lugardopezinho.velocityX = -2
     
     if (lugardopezinho.x < 0){
 
@@ -72,6 +114,8 @@ function draw(){
     Rexinho.collide(chaozinhoinvisivel);
 
     nuvenhador();
+
+    geraculador();
 
     drawSprites();
 
@@ -91,9 +135,57 @@ function nuvenhador(){
 
         Rexinho.depth=Rexinho.depth+1;
 
+        grupodanuvenzinha.add(umanuvenzinha);
+
         umanuvenzinha.y=Math.round(random(10,101));
 
         umanuvenzinha.lifetime = 250;
+
+    }
+
+
+}
+
+function geraculador(){
+
+    if (frameCount%60===0){
+
+        var umobstaculinho = createSprite (600, 165, 10, 40) ;
+
+        umobstaculinho.velocityX = -6;
+
+        var v1 = Math.round (random(1,6));
+
+        switch (v1){
+            case 1: umobstaculinho.addImage (b1);
+            break;
+
+            case 2: umobstaculinho.addImage (b2);
+            break;
+
+            case 3: umobstaculinho.addImage (b3);
+            break;
+
+            case 4: umobstaculinho.addImage (b4);
+            break;
+
+            case 5: umobstaculinho.addImage (b5);
+            break;
+
+            case 6: umobstaculinho.addImage (b6);
+            break;
+
+            default: break;
+
+        }
+
+        umobstaculinho.scale = 0.4;
+
+        grupodosobstaculos.add(umobstaculinho);
+
+        umobstaculinho.lifetime = 251;
+
+
 
     }
 
